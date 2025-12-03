5f4afef68c84d1801f337aa6d663f5ae5fd7893f
Lokesh Mali <malilokesh311@gmail.com>
2025-12-03 23:02:19 +0530

    Edit and create role

--- snapcart/src/components/EditRoleMobile.jsx
+++ snapcart/src/components/EditRoleMobile.jsx
@@ -9,7 +9,7 @@ function EditRoleMobile() {
         { id: "admin", label: "Admin", icon: UserCogIcon },
         { id: "deliveryBoy", label: "Delivery Boy", icon: Bike }
     ])
-    const [selectedRole , setSelectedRole]=useState("")
+    const [selectedRole, setSelectedRole] = useState("")
 
     return (
         <div className='flex flex-col min-h-screen p-6 w-full'>
@@ -29,20 +29,28 @@ function EditRoleMobile() {
                 }}
                 className='text-3xl md:text-4xl font-extrabold text-green-700 text-center mt-8'
             >Sele Your Role</motion.h1>
-            <div className='flex flex-col md:flex-row justify-center items-center gap-6  mt-10'>
-              {roles.map((role)=>{
-                const Icon =role.icon
-                const isSelected = selectedRole==role.id
-                return(
-                    <motion.div
-                     className={`flex flex-col items-center justify-center w-48 h-44 rounded-2xl border-2 transition-all ${isSelected?"border-green-600":"border-gray-300 bg-white hover:border-green-400"}`}
-                    >
-                        <Icon />
-                        <span>{role.label}</span>
-                    </motion.div>
-                )
-              })}
+            <div className='flex flex-col md:flex-row justify-center items-center gap-6 mt-10'>
+                {roles.map((role) => {
+                    const Icon = role.icon;
+                    const isSelected = selectedRole === role.id;
+
+                    return (
+                        <motion.div
+                        whileTap={{scale:0.96}}
+                        onClick={()=>setSelectedRole(role.id)}
+                            key={role.id}   
+                            className={`flex flex-col items-center justify-center w-48 h-44 rounded-2xl border-2 transition-all ${isSelected
+                                    ? "border-green-600"
+                                    : "border-gray-300 bg-white hover:border-green-400"
+                                }`}
+                        >
+                            <Icon />
+                            <span>{role.label}</span>
+                        </motion.div>
+                    );
+                })}
             </div>
+
 
         </div>
     )
